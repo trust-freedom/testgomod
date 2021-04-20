d2b5400f2f15e722447b502c7f1fd8058bb38cec
lili.sun <lili.sun@qunar.com>
2021-04-20 17:14:14 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trust-freedom/testgomod
+
+go 1.14
---
+++ testgomod.go
@@ -0,0 +1,8 @@
+package testgomod
+
+import "fmt"
+
+// SayHello returns a friendly greeting
+func SayHello(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
